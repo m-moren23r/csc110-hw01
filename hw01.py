a22b93ff7b7f3b36b2a233a65ecc998f9b25b237
m-moren23r <moren23r@mtholyoke.edu>
2026-09-25 10:43:32 -0400

    Final Version

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: (Rachel Moreno)
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,6 +45,19 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    result = ( (3 * x) - (9 * y) ) / ((2 * a) * (b - c) )
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result)
+    
 
     # End of Part 1 ----------------------
 
@@ -52,6 +65,12 @@ def main():
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
+    x = 5
+    y = -3
+    result = (x ** 2) * (y ** 4)
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result)
 
     # End of Part 2 ----------------------
 
@@ -60,6 +79,12 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
+    a = 100
+    b = 13
+    result = a // b
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result)
 
     # End of Part 3 ----------------------
 
@@ -67,6 +92,8 @@ def main():
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
+    result = a % b
+    print ("Part 4: result =", result)
 
     # End of Part 4 ----------------------
 
